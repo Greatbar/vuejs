6468d95f9650569101dd8ff2e10270c491efea7e
Greatbar <gratebar1@gmail.com>
2021-12-16 21:58:38 +0300

    all added

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="info">
+    <img src="../assets/Indeets.png" width="200" height="200"/>
+    <h1 class="First_Name">Turchenkov Vsevolod</h1>
+    <h2>BSBO-07-19</h2>
+    <p class="mes">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoCard"
+}
+</script>
+
+
+<style scoped>
+</style>
